d6d641221effddb4ba216685cbaedffd070ed8ff
harshgehlot-code <er.harsh.gehlot1@gmail.com>
2025-12-26 19:15:10 +0530

    add Accounts admin.py

---
+++ accounts/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import *
+
+admin.site.register(HotelUser)
+
+admin.site.register(HotelVendor)
+
+admin.site.register(Hotel)
+
+admin.site.register(Ameneties)
+
+admin.site.register(HotelImages)
